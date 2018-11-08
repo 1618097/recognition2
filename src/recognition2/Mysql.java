da16e142c3e265ea9193156ffe441468c28a99fc
1618097 <1618097@cs16097>
2018-11-08 15:53:21 +0900

    recognition2

--- src/recognition2/Mysql.java
+++ src/recognition2/Mysql.java
@@ -1,86 +1,66 @@
 package recognition2;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Mysql 
-{
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class Mysql {
 	String driver;// JDBCドライバの登録
     String server, dbname, url, user, password;// データベースの指定
     Connection con;
-    static Statement stmt;
+    Statement stmt;
     Map<String, Object> lng = new HashMap<>();
     private String id;
-    
-	public Mysql() 
-	{
-		this.driver		= "org.gjt.mm.mysql.Driver";
-        this.server 	= "sangi2018.sist.ac.jp";
-        this.dbname 	= "sangi2018";
-        this.url		= "jdbc:mysql://" + server + "/" + dbname + "?useUnicode=true&characterEncoding=UTF-8";
-        this.user		= "sangi2018";
-        this.password	= "sistsangi2018";
-        this.id			= "1618097";
+
+	public Mysql() {
+		this.driver = "org.gjt.mm.mysql.Driver";
+		this.server = "sangi2018.sist.ac.jp";
+		this.dbname = "sangi2018";
+		this.url = "jdbc:mysql://" + server + "/" + dbname + "?useUnicode=true&characterEncoding=UTF-8";
+        this.user = "sangi2018";
+        this.password = "sistsangi2018";
+        this.id = "1618097";
         
-        try 
-        {
+        try {
             this.con = DriverManager.getConnection(url, user, password);
             this.stmt = con.createStatement ();
-        } 
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        
-        try
-        {
+        try {
             Class.forName (driver);
-        } 
-        catch (ClassNotFoundException e) 
-        {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 	}
-		
-	public ResultSet getID() 
-	{
+	
+	public ResultSet getID() {
 		ResultSet rs = null;
 		String sql = "SELECT * FROM  `images` WHERE  `age_min` = -1";
-		try 
-		{
+		try {
 			rs = stmt.executeQuery (sql);
-		} 
-		catch (SQLException e) 
-		{
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return rs;
 	}
 	
-	public static void updateImage(String class1, double score1, String class2, double score2, String class3,double score3) {
-		// TODO Auto-generated method stub
+	public void updateImage(JsonNode text1,JsonNode score1,JsonNode text2,JsonNode score2,JsonNode text3,JsonNode score3) {
 		StringBuffer buf = new StringBuffer();
-		buf.append("INSERT INTO `recognitions`(`user_id`, `class1`, `socre1`, `class2`, `score2`, `class3`, `score3`) VALUES (1618097," + class1 +","+ score1 +","+ class2 +","+ score2 +","+ class3 +","+ score3 +");");
+		buf.append("INSERT INTO  `recognitions` (`user_id`, `class1`, `score1`, `class2`, `score2`, `class3`, `score3` ) VALUES ("+ id+ ","+ text1 +","+ score1+","+text2+","+score2+","+text3+","+score3+");");
 		String sql = buf.toString();
-		try 
-		{
+		try {
 			stmt.execute (sql);
-		} 
-		catch (SQLException e) 
-		{
-			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-		
 }
--- src/recognition2/Recognition2_lib.java
+++ src/recognition2/Recognition2_lib.java
@@ -14,6 +14,7 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
 
+import recognition2.Mysql;
 
 public class Recognition2_lib {
 	VisualRecognition service;
@@ -24,30 +25,28 @@ public class Recognition2_lib {
 	public Recognition2_lib(){	
 		service = new VisualRecognition("2018-03-19");
 		iamOptions = new IamOptions.Builder()
-			    .apiKey("GfONvmQN0oReyT9Nc8Yk3n30lgC5RxsGDZVGZ2f_fCWB")
+			    .apiKey("1618097")
 			    .build();
 		service.setIamCredentials(iamOptions);
 	}
 	
-	public void recognition(String file) {
-		// TODO Auto-generated method stub
+	public void file(File file) {
 		try {
-			imagesStream = new FileInputStream("img/image.jpg");
+			imagesStream = new FileInputStream(file);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	
-	public JsonNode recongnition2() {
+	public void view() {
 		ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
 				  .imagesFile(imagesStream)
 				  .imagesFilename("image.jpg")
 				  .threshold((float) 0.6)
 				  .owners(Arrays.asList("IBM"))
 				  .build();
-		result = service.classify(classifyOptions).execute();
+		ClassifiedImages result = service.classify(classifyOptions).execute();
 		
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode node = null;
@@ -56,23 +55,18 @@ public class Recognition2_lib {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			}
-		return node;
 		}
-	public void recognition_json(JsonNode node) {
-	String class1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("class").toString();
-	double score1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("score").doubleValue();
-	String class2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class").toString();
-	double score2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("score").doubleValue();
-	String class3 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("class").toString();
-	double score3 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("score").doubleValue();
-	System.out.println(class2);
-	Mysql mysql = new Mysql();
-	Mysql.updateImage(class1,score1,class2,score2,class3,score3);
+		
+		JsonNode text1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("class");
+		JsonNode text2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class");
+		JsonNode text3 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("class");
+		JsonNode score1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("score");
+		JsonNode score2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("score");
+		JsonNode score3 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("score");
+		//System.out.println(result);
+		//System.out.println(node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class"));
+		Mysql mysql = new Mysql();
+		mysql.updateImage(text1,score1,text2,score2,text3,score3);
 	}
-	
+
 }
-
-	
-
-
